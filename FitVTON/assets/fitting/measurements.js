d0fa614fdb1ec71b77ccc58080a637e759214c53
root <root@notebook-rn-202606121621110597296-tqk1q-0.notebook-rn-202606121621110597296-tqk1q.namespace-203.svc.cluster.local>
2026-06-12 19:38:29 +0000

    r

---
+++ FitVTON/assets/fitting/measurements.js
@@ -0,0 +1,62 @@
+window.FE_MEASURES = {
+ "f0": {
+  "height": 165,
+  "chest": 80,
+  "waist": 64,
+  "bust": 89
+ },
+ "f1": {
+  "height": 170,
+  "chest": 79,
+  "waist": 70,
+  "bust": 88
+ },
+ "f2": {
+  "height": 160,
+  "chest": 95,
+  "waist": 83,
+  "bust": 97
+ },
+ "f3": {
+  "height": 175,
+  "chest": 109,
+  "waist": 92,
+  "bust": 114
+ },
+ "f4": {
+  "height": 155,
+  "chest": 90,
+  "waist": 83,
+  "bust": 94
+ },
+ "m0": {
+  "height": 172,
+  "chest": 104,
+  "waist": 102,
+  "bust": 105
+ },
+ "m1": {
+  "height": 183,
+  "chest": 92,
+  "waist": 84,
+  "bust": 106
+ },
+ "m2": {
+  "height": 184,
+  "chest": 93,
+  "waist": 84,
+  "bust": 97
+ },
+ "m3": {
+  "height": 170,
+  "chest": 99,
+  "waist": 92,
+  "bust": 100
+ },
+ "m4": {
+  "height": 178,
+  "chest": 90,
+  "waist": 78,
+  "bust": 102
+ }
+};
